03b4b353ee92f322dd9f7f794132f7dc95aae00b
phatB1910276 <phatB1910276@student.ctu.edu.vn>
2023-04-17 14:30:10 +0700

    chuc nang tim kiem

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import '../utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -15,68 +14,76 @@ class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController searchController = TextEditingController();
   bool showUser = false;
 
-  @override
-  void dispose() {
-    super.dispose();
-    searchController.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  //   searchController.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          leading: const Icon(Icons.search, color: primaryColor),
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarColor: mobileBackgroundColor,
-          ),
-          toolbarHeight: 70,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: TextFormField(
-            controller: searchController,
-            decoration: const InputDecoration(
-              labelText: 'Tìm kiếm người dùng...',
-              //labelStyle: TextStyle(color: primaryColor),
+        appBar: AppBar(
+            leading: const Icon(Icons.search, color: primaryColor),
+            systemOverlayStyle: const SystemUiOverlayStyle(
+              statusBarColor: mobileBackgroundColor,
             ),
-            onFieldSubmitted: (value) {
-              setState(() {
-                print(searchController);
-                showUser = true;
-              });
-              // print(searchController);
-            },
-          )),
-      body: showUser
-          ? FutureBuilder(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .where('username',
-                      isGreaterThanOrEqualTo: searchController.text)
-                  .get(),
-              builder: (context,
-                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(
-                      color: themeColor,
-                    ),
-                  );
-                }
-                return ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(snapshot.data!.docs[index]['avtUrl']),
-                      ),
-                      title: Text(snapshot.data!.docs[index]['username']),
-                    );
-                  },
-                );
+            toolbarHeight: 70,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: TextFormField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                labelText: 'Tìm kiếm người dùng...',
+                //labelStyle: TextStyle(color: primaryColor),
+              ),
+              onFieldSubmitted: (String _) {
+                setState(() {
+                  //print(searchController);
+                  showUser = true;
+                });
+                //print(searchController.text);
+                // print(searchController);
               },
-            )
-          : Text('post'),
-    );
+            )),
+        body: showUser
+            ? FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .where('username'.toLowerCase(),
+                        isGreaterThanOrEqualTo:
+                            searchController.text.toLowerCase())
+                    .get(),
+                builder: (context,
+                    //AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                    snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(
+                        color: themeColor,
+                      ),
+                    );
+                  }
+                  return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                          onTap: () {},
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  (snapshot.data! as dynamic).docs[index]
+                                      ['avtUrl']),
+                            ),
+                            title: Text((snapshot.data! as dynamic).docs[index]
+                                ['username']),
+                          ));
+                    },
+                  );
+                },
+              )
+            : const Center(
+                child: Text('Tìm kiếm người dùng'),
+              ));
   }
 }
