a6c11f3adbd4a4a4b30ce9a74f92b82187706c46
phatB1910276 <phatB1910276@student.ctu.edu.vn>
2023-04-27 02:16:24 +0700

    ra soat cac loi va cac problems con lai

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Socical Network',
-          theme: ThemeData(fontFamily: 'Futura').copyWith(
+          theme: ThemeData().copyWith(
             scaffoldBackgroundColor: mobileBackgroundColor,
           ),
           // home: ResponsiveScreen(
